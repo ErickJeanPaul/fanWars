591b24f740956d03eecf13a939a32127ad1c15ae
Laura Camila Quimbaya <camila111paco@gmail.com>
2022-01-29 22:34:43 -0500

    cambiosmejora llamada a la api

--- js/index.js
+++ js/index.js
@@ -4,17 +4,26 @@ let img3 = document.getElementById("img3")
 
 
 
-fetch('https://rickandmortyapi.com/api/character/')
+fetch('https://rickandmortyapi.com/api/character')
   .then( (response) =>{
     return response.json()
   })
   .then((data)=>{
-    console.log(data)
+    console.log(data.results[0])
     const carrouData = (ima) => {
       let min = 1
-      let max = 800
+      let max = 20
       let i = Math.floor(Math.random() * (max - min) ) + min;
-      ima.innerHTML = `<img src="https://rickandmortyapi.com/api/character/avatar/${i}.jpeg" class="d-block w-100 img-carrou" alt="..." >
+      let rutImag = data.results[i].image
+      let nameIm = data.results[i].name
+      let specieInfo = data.results[i].species
+      let statusInfo = data.results[i].status
+      ima.innerHTML = `<img src="${rutImag}" class="d-block w-100 img-carrou" alt="..." >
+                      <div class="carousel-caption d-none d-md-block">
+                      <h2 class="text-carousel">${nameIm}</h2>
+                      <p class="text-carousel">Especie: ${specieInfo}</p>
+                      <p class="text-carousel">Estado: ${statusInfo}</p>
+                      </div>
                       
       `
     }
