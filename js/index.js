e0eef085c452760558929b0251565d8e53730f58
Laura Camila Quimbaya <camila111paco@gmail.com>
2022-01-29 21:50:11 -0500

    cambios pagina

--- js/index.js
+++ js/index.js
@@ -1,11 +1,27 @@
+let img1 = document.getElementById("img1")
+let img2 = document.getElementById("img2")
+let img3 = document.getElementById("img3")
 
-const BASE_API_URL = "https://rickandmortyapi.com/";
 
-const container = document.querySelector("#container"); 
-const ul = document.createElement("ul");
 
-fetch( `${ BASE_API_URL }/character` )
-.then( (response) => {
-    console.log( response );
-    return response.json();
-})
+fetch('https://rickandmortyapi.com/api/character/')
+  .then( (response) =>{
+    return response.json()
+  })
+  .then((data)=>{
+    console.log(data)
+    const carrouData = (ima) => {
+      let min = 1
+      let max = 800
+      let i = Math.floor(Math.random() * (max - min) ) + min;
+      ima.innerHTML = `<img src="https://rickandmortyapi.com/api/character/avatar/${i}.jpeg" class="d-block w-100 img-carrou" alt="..." >
+                      
+      `
+    }
+    carrouData(img1)
+    carrouData(img2)
+    carrouData(img3)
+  })
+  .catch((error)=>{
+    console.log(error)
+  })
