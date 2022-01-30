012473690ad0bf78914a927462b929c042191d2c
ErickJeanPaul <anonymousvi16@gmail.com>
2022-01-30 03:24:16 -0500

    episodios

--- js/index.js
+++ js/index.js
@@ -5,23 +5,23 @@ let img3 = document.getElementById("img3")
 
 
 fetch('https://rickandmortyapi.com/api/character/')
-  .then( (response) =>{
-    return response.json()
-  })
-  .then((data)=>{
-    console.log(data)
-    const carrouData = (ima) => {
-      let min = 1
-      let max = 800
-      let i = Math.floor(Math.random() * (max - min) ) + min;
-      ima.innerHTML = `<img src="https://rickandmortyapi.com/api/character/avatar/${i}.jpeg" class="d-block w-100 img-carrou" alt="..." >
-                      
-      `
-    }
-    carrouData(img1)
-    carrouData(img2)
-    carrouData(img3)
-  })
-  .catch((error)=>{
-    console.log(error)
-  })
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data)
+        const carrouData = (ima) => {
+            let min = 1
+            let max = 800
+            let i = Math.floor(Math.random() * (max - min)) + min;
+            ima.innerHTML = `<img src="https://rickandmortyapi.com/api/character/avatar/${i}.jpeg" class="d-block w-100 img-carrou" alt="..." >`
+        }
+        const incremento = 0;
+        incremento++;
+        incremento != 10 ?
+        carrouData(img2):
+        console.log("hola");
+    })
+    .catch((error) => {
+        console.log(error)
+    })
